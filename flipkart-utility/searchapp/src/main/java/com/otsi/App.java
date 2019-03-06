630f574f20803cb7c04371c10a22b5a21728076b
praveen.hemadri <praveen.hemadri@otsi.co.in>
2019-03-06 15:11:43 +0530

    Initial Commit

---
+++ flipkart-utility/searchapp/src/main/java/com/otsi/App.java
@@ -0,0 +1,13 @@
+package com.otsi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
